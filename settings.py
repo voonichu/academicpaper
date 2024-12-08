c6618abb52401c6ee14d85f599f1cd7b11976687
Dylan Edwards <106411648+voonichu@users.noreply.github.com>
2024-12-08 16:41:05 -0500

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,6 @@
+SEARCH_KEY = "AIzaSyCaHySwHWek5qG5FUCPBOuvHhKN-lomkjI"
+SEARCH_ID = "114ad9ab7b65245ff"
+COUNTRY = "us"
+
+SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&num=10&gl=" + COUNTRY
+RESULT_COUNT = 20
